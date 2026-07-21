0902220d44eb23ff637062edb91eede645d9a321
shiinayane <shiinayane@gmail.com>
2026-07-21 14:12:54 +0900

    Initial Commit

---
+++ BiliKitMac/BiliKitMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiliKitMacApp.swift
+//  BiliKitMac
+//
+//  Created by shiinayane on 2026/07/21.
+//
+
+import SwiftUI
+
+@main
+struct BiliKitMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiliKitMacTests/BiliKitMacTests.swift
@@ -0,0 +1,19 @@
+//
+//  BiliKitMacTests.swift
+//  BiliKitMacTests
+//
+//  Created by shiinayane on 2026/07/21.
+//
+
+import Testing
+@testable import BiliKitMac
+
+struct BiliKitMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
